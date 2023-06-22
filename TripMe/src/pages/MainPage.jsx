58de9d19fc4b50949354954208c22c7ea2633e0f
Danny <chungdanny56@gmail.com>
2023-06-22 14:09:23 -0700

    Initial commit, got mapbox set up in React

---
+++ TripMe/src/pages/MainPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Mapbox from '../components/Mapbox';
+
+
+export default function MainPage (){
+    return (
+        <div>
+            <Mapbox />
+        </div>
+    );
+};
