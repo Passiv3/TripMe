8773b4e769f48007f1d728e2772ef03b56f47547
Danny <chungdanny56@gmail.com>
2023-06-30 15:51:09 -0700

    New Branch to add a form to the side of the map

---
+++ TripMe/src/components/Form.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Form(){
+    return(<form>
+        <fieldset>
+            <input type = "dropdown"></input>
+        </fieldset>
+    </form>);
+}
